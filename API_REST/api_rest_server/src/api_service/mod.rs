a7e1725eb946c81b188c8472f02e3fcca98df80e
AlexLeo99 <baker-plan-b@hotmail.com>
2021-06-07 17:59:04 -0500

    Coments

--- API_REST/api_rest_server/src/api_service/mod.rs
+++ API_REST/api_rest_server/src/api_service/mod.rs
@@ -1,24 +1,26 @@
+// External imports
 use bson::{doc, Document};
 use mongodb::results::{DeleteResult, UpdateResult, InsertOneResult};
 use mongodb::{error::Error, Collection};
 use serde::{Deserialize, Serialize};
-
+// External constructors
 extern crate serde;
 extern crate serde_json;
 
+// Estructure data for DB
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Data {
     pub title: String,
     pub author: String,
 }
 
+// Reference colection clone
 #[derive(Clone)]
 pub struct ApiService {
     collection: Collection,
 }
 
-
-/// Transform user to mongo db document
+// Transform data to mongo db document
 fn data_to_document(data: &Data) -> Document {
     let Data {
         title,
@@ -30,27 +32,24 @@ fn data_to_document(data: &Data) -> Document {
     }
 }
 
+// Functions with quieries to Mongo
 impl ApiService {
     pub fn new(collection: Collection) -> ApiService {
         ApiService { collection }
     }
 
-    /// Insert user in mongo db (user)
+    // Insert data to Mongo DB
     pub fn create(&self, _data:&Data) -> Result<InsertOneResult, Error> {
         self.collection.insert_one(data_to_document(_data), None)
     }
 
-    /// Update existing user in mongo db (email)
+    // Update an existing document 
     pub fn update(&self, _data:&Data, _param: &String) -> Result<UpdateResult, Error> {
-        // let Data {
-        //     title: _title,
-        //     author: _author,
-        // } = _data;
         let object_param = bson::oid::ObjectId::with_string(_param).unwrap();
         self.collection.update_one(doc! { "_id": object_param }, data_to_document(_data), None)
     }
 
-    /// Delete existing user in mongo db (email)
+    // Delete some document
     pub fn delete(&self, _title: &String) -> Result<DeleteResult, Error> {
         self.collection.delete_one(doc! { "title": _title }, None)
     }
@@ -62,7 +61,7 @@ impl ApiService {
         Ok(docs)
     }
 
-    /// Retrieve parameter
+    // Get documents with quiery
     pub fn get_by(&self, param: &String) -> std::result::Result<std::vec::Vec<bson::ordered::OrderedDocument>, mongodb::error::Error> {
         let cursor = self.collection.find(doc! { "author": { "$regex": param } }, None).ok().expect("Failed to execute find.");
         let docs: Vec<_> = cursor.map(|doc| doc.unwrap()).collect();
--- API_REST/api_rest_server/src/main.rs
+++ API_REST/api_rest_server/src/main.rs
@@ -1,3 +1,4 @@
+// External imports
 use actix_cors::Cors;
 use actix_web::{http, middleware, App, HttpServer};
 use dotenv::dotenv;
@@ -5,19 +6,23 @@ use mongodb::{options::ClientOptions, Client};
 use std::env;
 use api_service::ApiService;
 
+// External modules reference
 mod api_router;
 mod api_service;
 
+// Api Service constructor
 pub struct ServiceManager {
     api: ApiService,
 }
 
+// Api Servie Implementation
 impl ServiceManager {
     pub fn new(api: ApiService) -> Self {
         ServiceManager { api }
     }
 }
 
+// Service Manager constructor
 pub struct AppState {
     service_manager: ServiceManager,
 }
@@ -32,24 +37,24 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     // Parse a connection string into an options struct.
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE URL is not in .env file");
     let client_options = ClientOptions::parse(&database_url).unwrap();
 
-    // Get a handle to the deployment.
+    // Get the reference to Mongo DB
     let client = Client::with_options(client_options).unwrap();
 
-    // Get a handle to a database.
-    let database_name = env::var("DATABASE_NAME").expect("DATABASE_NAME is not set in .env file");
+    // get the reference to the Data Base
+    let database_name = env::var("DATABASE_NAME").expect("DATABASE NAME is not in .env file");
     let db = client.database(&database_name);
 
-    // Get a handle to a collection in the database.
-    let user_collection_name = env::var("USER_COLLECTION_NAME").expect("USER_COLLECTION_NAME is not set in .env file");
+    // get the reference to the Collection
+    let user_collection_name = env::var("USER_COLLECTION_NAME").expect("COLLECTION NAME is not in .env file");
     let user_collection = db.collection(&user_collection_name);
 
-    // server url
-    let server_url = env::var("SERVER_URL").expect("SERVER_URL is not set in .env file");
+    // Gte the server URL
+    let server_url = env::var("SERVER_URL").expect("SERVER URL is not in .env file");
 
-    // start server
+    // Start the server
     HttpServer::new(move || {
         let user_service_worker = ApiService::new(user_collection.clone());
         let service_manager = ServiceManager::new(user_service_worker);
@@ -62,7 +67,7 @@ async fn main() -> std::io::Result<()> {
             .max_age(3600)
             .finish();
 
-        // launch http server
+        // Init http server
         App::new()
             .wrap(cors_middleware)
             .wrap(middleware::Logger::default())
